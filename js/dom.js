82dd5dadc01d8a1a5b58e2e55b4609fe7dfce2e7
Tonmoy <tonmoy@Tonmoy.Tonmoy>
2025-11-02 01:24:41 +0600

    Basic DOM

---
+++ js/dom.js
@@ -0,0 +1,17 @@
+
+const header = document.getElementById('header');
+const section = document.createElement('section');
+
+section.innerHTML = `
+
+<h1>My Dress</h1>
+
+<ul>
+<li>Shirt</li>
+<li>Pant</li>
+<li>T-Shirt</li>
+<li>Shoe</li>
+
+</ul>
+`
+header.appendChild(section);
